beaf25bb5c68b321bceb551ebc099c35706c3047
Khoi Nguyen <khnaero@gmail.com>
2017-11-01 13:46:59 -0500

    Add public profile list

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   # exposes only actions for user_profile and not user_
-  # resources :users, only: [] do
   resources :users do
     resource :profile, only: [:index, :edit, :show, :update]
   end

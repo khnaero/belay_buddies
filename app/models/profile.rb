bf2216e3cc3a64553a03c95d3d32ff49f3d135c1
Khoi Nguyen <khnaero@gmail.com>
2017-11-01 20:14:12 -0500

    Switch country code to full country name

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :reviews
+
+  # because 'country' is the column name, must change other variables
+  def country_name
+    countryn = ISO3166::Country[country]
+    countryn.translations[I18n.locale.to_s] || countryn.name
+  end
 end
